5189d8ebafcb3ccec20ac244075c4001cf422a3f
Feichi Lu <feiclu@student.ethz.ch>
2022-09-08 16:25:16 +0200

    readme

--- thingspeak/api.py
+++ thingspeak/api.py
@@ -6,7 +6,7 @@ def send_data(temp, humidity):
     #headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     requests.get("https://api.thingspeak.com/update", params={'field1': temp, 'field2': humidity, 'api_key':'OM2Q7PTXDBD4OOSQ'})
 
-for i in range(10):
+for i in range(1000):
     temp = np.random.uniform(10,40)
     humidity = np.random.uniform(10,90)
     print(temp, humidity)
